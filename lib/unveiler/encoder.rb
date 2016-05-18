908a3b6149c80cc65ba75f8f7a4b66a66660261e
Brendan Goodenough <brendan@goodenough.nz>
2016-05-18 21:52:28 +1200

    Refactor the encoder to be more intuitive

--- lib/unveiler/encoder.rb
+++ lib/unveiler/encoder.rb
@@ -7,38 +7,36 @@ class Encoder
   # specified output file location.
   def encode(input_file, output_file, data)
     data = Base64.encode64(data)
-    data_lsb = data.unpack("B*").first
+    data = data.unpack("B*").first
 
-    input_file_lsb = get_input_file_bytes(input_file)
-    replace_lsb(input_file_lsb, data_lsb)
-    write_output_file(input_file_lsb, output_file)
+    bytes = get_input_file_bytes(input_file)
+    replace_bytes(bytes, data)
+    write_output_file(bytes, output_file)
 
     puts "Successfully encoded #{output_file}!"
   end
 
+  # Writes the specified bytes to the output file by first converting the binary
+  # to UTF-8 encoding.
   def write_output_file(bytes, output_file)
     puts "Writing #{output_file}"
 
-    bytes.map! do |byte|
-      byte = byte.to_i(2)
-    end
-
+    bytes.map!{|byte| byte = byte.to_i(2)}
     bytes = bytes.pack("C*").force_encoding('utf-8')
 
-    File.open(output_file, 'w') do |file|
-      file.write(bytes)
-    end
+    File.open(output_file, 'w'){|file| file.write(bytes)}
   end
 
-  # Loops through the array of bytes from the input file, replacing the least
-  # significant bit with the next sequential bit from the data array.
-  def replace_lsb(input_file_lsb, data_lsb)
-    puts "Encoding data"
+  # Loops through the specified bytes array, replacing the least significant
+  # bits with the following bit from the data. The loop is terminated when
+  # either all bytes in the array have been iterated over, or the end of the
+  # data string has been reached.
+  def replace_bytes(bytes, data)
     count = 0
-    input_file_lsb.reverse.each do |byte|
-      byte[7] = data_lsb[count]
+    bytes.reverse.each do |byte|
+      byte[7] = data[count]
       count += 1
-      break if count == data_lsb.length
+      break if count == data.length
     end
   end
 
