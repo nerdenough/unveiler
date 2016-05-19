606ab6c74b0518a6f9780c6c50e07ec090bd0ef8
Brendan Goodenough <brendan@goodenough.nz>
2016-05-19 22:22:10 +1200

    Refactor and add decent documentation for unveiler
    Encoder and Decoder classes have been deleted, and the methods have
    been refactored into Unveiler. This is to keep the code clean, simple
    and intuitive. All file reading and writing has been moved to the bin
    folder in order to avoid obstructing use of unveiler for other purposes.
    
    Tests need to be written for the remaining Unveiler methods.

--- lib/unveiler.rb
+++ lib/unveiler.rb
@@ -1,44 +1,112 @@
-require_relative "unveiler/encoder"
-require_relative "unveiler/decoder"
+require "base64"
+
+# Unveiler is a basic steganography tool that encodes data by manipulating
+# existing binary data and can decode previously encoded data.
+#
+# Author::      Brendan Goodenough
+# Copyright::   Copyright (c) 2016 Brendan Goodenough
+# License::     MIT
+#
+# The encoding of the data is achieved by manipulating the least significant
+# bits of each byte in the target data. These bits are replaced with the next
+# sequential bit of the data to be encoded, looping until the end of the data
+# is reached.
+#
+# Decoding is achieved by looping through each byte of the target data, and
+# stringing together the least significant bits of each byte. This is repeated
+# until the last 3 bytes in the string of bits forms the phrase "EOF".
 
 class Unveiler
-  def self.encode(input_file, output_file, data)
-    encoder = Encoder.new
-    encoder.encode(input_file, output_file, data)
-  end
+  # Encodes the specified string of data into the target data. Upon success,
+  # a UTF-8 string containing the manipulated data will be returned.
+  #
+  # +target+::  Target data to be manipulated
+  # +data+::    Data to encode
+  def encode(target, data)
+    # Convert target data into an array of bytes
+    bytes = target.unpack("B*")[0].scan(/.{8}/)
 
-  def self.decode(input_file, output_file)
-    decoder = Decoder.new
-    decoder.decode(input_file, output_file)
-  end
+    # Convert data to binary
+    data = Base64.encode64(data)
+    data += "EOF"
+    data = data.unpack("B*").first
+    
+    # Encode the data into the array of bytes
+    bytes = manipulate_bytes(bytes, data)
 
-  # Reads the input file and returns an array of bytes. If the file does not
-  # exist, an error is printed to the terminal and the program terminates.
-  def self.get_file_bytes(file_name)
-    if File.exist?(file_name)
-      File.open(file_name, "rb") do |file|
-        bytes = file.read.unpack("B*")
-        return bytes[0].scan(/.{8}/)
-      end
-    else
-      puts "Error: #{file_name} does not exist!"
-      exit
+    # Convert binary to UTF-8
+    bytes.map!{|byte| byte = byte.to_i(2)}
+    return bytes.pack("C*").force_encoding("utf-8")
+  end
+  
+  # Decodes any data within the specified target data. Upon success, the decoded
+  # string will be returned. If an error is thrown while processing the data,
+  # nil will be returned.
+  #
+  # +target+::  Target data to be decoded
+  def decode(target)
+    begin
+      # Convert the target data into an array of bytes
+      bytes = target.unpack("B*")[0].scan(/.{8}/)
+      
+      # Obtain string from the bytes
+      bytes = process_bytes(bytes)
+      return Base64.decode64(bytes)
+    rescue
+      return nil
     end
   end
-
-  # Writes the specified bytes to the output file by first converting the binary
-  # to UTF-8 encoding.
-  def self.write_bytes(bytes, file_name)
-    puts "Writing #{file_name}"
-
-    bytes.map!{|byte| byte = byte.to_i(2)}
-    bytes = bytes.pack("C*").force_encoding("utf-8")
-
-    File.open(file_name, "w"){|file| file.write(bytes)}
+  
+  # Manipulates an array of bytes by looping through and replacing the least
+  # significant bit of each byte with the next sequential bit in a string of
+  # binary data. The loop will break when the end of the data string is
+  # reached. Returns the manipulated array of bytes.
+  #
+  # +bytes+:: Array of bytes to manipulate
+  # +data+::  Binary data to insert
+  def manipulate_bytes(bytes, data)
+    count = 0
+    bytes.reverse.each do |byte|
+      byte[7] = data[count]
+      count += 1
+      break if count == data.length
+    end
+    return bytes
   end
+  
+  # Processes the specified array of bytes by looping through each byte and
+  # appending the least significant bit onto a string of bits. At the end of
+  # every eighth bit, a check will be made to see if the last 3 bytes of the
+  # string contain the phrase "EOF". Only the bytes before EOF will be returned
+  # if the phrase is found. An error will be raised if no EOF is found. 
+  #
+  # +bytes+:: Array of bytes to be processed
+  def process_bytes(bytes)
+    bits = ""
+    bytes.reverse.each do |byte|
+      # Least significant bit
+      bits += byte[7]
 
-  def self.write_file(data, file_name)
-    puts "Writing #{file_name}"
-    File.open(file_name, "w"){|file| file.write(data)}
+      # Check whether a full byte has been read
+      if bits.length % 8 == 0
+        # Convert bits to array of bytes
+        data = bits.scan(/.{8}/)
+        
+        if data.length >= 3
+          # Convert bytes to a UTF-8 string
+          data.map!{|byte| byte = byte.to_i(2)}
+          data = data.pack("C*").force_encoding("utf-8")
+          len = data.length
+          
+          if data[-3,3] == "EOF"
+            # Return the UTF-8 string, excluding "EOF"
+            return data[0..len - 3]
+          end
+        end
+      end
+    end
+
+    # No EOF was found, error
+    raise "No phrase matching 'EOF' was found"
   end
 end
--- spec/manipulate_spec.rb
+++ spec/manipulate_spec.rb
@@ -1,36 +1,36 @@
 require "unveiler"
 
-RSpec.describe Encoder, "#replace_bytes" do
+RSpec.describe Unveiler, "#manipulate_bytes" do
   context "with equal number of bytes in string and array" do
     it "replaces the least significant bits with 0" do
-      encoder = Encoder.new
+      unveiler = Unveiler.new
       data = "0000"
 
       array = ["10001000", "10101010", "11111111", "11111111"]
       answer = ["10001000", "10101010", "11111110", "11111110"]
-      result = encoder.replace_bytes(array, data)
+      result = unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
     end
     it "replaces the least significant bits with 1" do
-      encoder = Encoder.new
+      unveiler = Unveiler.new
       data = "1111"
 
       array = ["10001000", "10101010", "11111111", "11111110"]
       answer = ["10001001", "10101011", "11111111", "11111111"]
-      result = encoder.replace_bytes(array, data)
+      result = unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
     end
     it "replaces the least significant bits with differing values" do
-      encoder = Encoder.new
+      unveiler = Unveiler.new
       data = "1010"
 
       array = ["10001000", "10101010", "11111111", "11111110"]
       answer = ["10001000", "10101011", "11111110", "11111111"]
-      result = encoder.replace_bytes(array, data)
+      result = unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
@@ -39,34 +39,34 @@ RSpec.describe Encoder, "#replace_bytes" do
 
   context "with a larger array length than the data string" do
     it "replaces the least significant bits with 0" do
-      encoder = Encoder.new
+      unveiler = Unveiler.new
       data = "00"
 
       array = ["10001001", "10101011", "11111111", "11111111"]
       answer = ["10001001", "10101011", "11111110", "11111110"]
-      result = encoder.replace_bytes(array, data)
+      result = unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
     end
     it "replaces the least significant bits with 1" do
-      encoder = Encoder.new
+      unveiler = Unveiler.new
       data = "11"
 
       array = ["10001000", "10101010", "11111111", "11111111"]
       answer = ["10001000", "10101010", "11111111", "11111111"]
-      result = encoder.replace_bytes(array, data)
+      result = unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
     end
     it "replaces the least significant bits with differing values" do
-      encoder = Encoder.new
+      unveiler = Unveiler.new
       data = "10"
 
       array = ["10001001", "10101010", "11111111", "11111110"]
       answer = ["10001001", "10101010", "11111110", "11111111"]
-      result = encoder.replace_bytes(array, data)
+      result = unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
@@ -75,34 +75,34 @@ RSpec.describe Encoder, "#replace_bytes" do
 
   context "with a longer data string than the array length" do
     it "replaces the least significant bits with 0" do
-      encoder = Encoder.new
+      unveiler = Unveiler.new
       data = "000000"
 
       array = ["11111111", "11111111", "11111111", "11111111"]
       answer = ["11111110", "11111110", "11111110", "11111110"]
-      result = encoder.replace_bytes(array, data)
+      result = unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
     end
     it "replaces the least significant bits with 1" do
-      encoder = Encoder.new
+      unveiler = Unveiler.new
       data = "111111"
 
       array = ["11111110", "11111110", "11111110", "11111110"]
       answer = ["11111111", "11111111", "11111111", "11111111"]
-      result = encoder.replace_bytes(array, data)
+      result = unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
     end
     it "replaces the least significant bits with differing values" do
-      encoder = Encoder.new
+      unveiler = Unveiler.new
       data = "100101"
 
       array = ["10001000", "10101011", "11111111", "11111110"]
       answer = ["10001001", "10101010", "11111110", "11111111"]
-      result = encoder.replace_bytes(array, data)
+      result = unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
