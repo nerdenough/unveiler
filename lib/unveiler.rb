650d377f1bc8bd0a86106836c218e402de6aad69
Brendan Goodenough <brendan@goodenough.nz>
2016-05-20 18:27:23 +1200

    Refactor unveiler to use static methods
    This allows use of all Unveiler methods without having to instantiate
    the class.
    
    I have replaced most double quotes with single quotes (unless required)
    as they were getting on my nerves. I could argue that the minimal
    performance gain by this change was the reason, but meh.

--- lib/unveiler.rb
+++ lib/unveiler.rb
@@ -1,4 +1,4 @@
-require "base64"
+require 'base64'
 
 # Unveiler is a basic steganography tool that encodes data by manipulating
 # existing binary data and can decode previously encoded data.
@@ -14,7 +14,7 @@ require "base64"
 #
 # Decoding is achieved by looping through each byte of the target data, and
 # stringing together the least significant bits of each byte. This is repeated
-# until the last 3 bytes in the string of bits forms the phrase "EOF".
+# until the last 3 bytes in the string of bits forms the phrase 'EOF'.
 
 class Unveiler
   # Encodes the specified string of data into the target data. Upon success,
@@ -22,33 +22,33 @@ class Unveiler
   #
   # +target+::  Target data to be manipulated
   # +data+::    Data to encode
-  def encode(target, data)
+  def self.encode(target, data)
     # Convert target data into an array of bytes
-    bytes = target.unpack("B*")[0].scan(/.{8}/)
+    bytes = target.unpack('B*')[0].scan(/.{8}/)
 
     # Convert data to binary
     data = Base64.encode64(data)
-    data += "EOF"
-    data = data.unpack("B*").first
+    data += 'EOF'
+    data = data.unpack('B*').first
 
     # Encode the data into the array of bytes
-    bytes = manipulate_bytes(bytes, data)
+    bytes = self.manipulate_bytes(bytes, data)
 
     # Convert binary to UTF-8
     bytes.map!{|byte| byte = byte.to_i(2)}
-    return bytes.pack("C*").force_encoding("utf-8")
+    return bytes.pack('C*').force_encoding('utf-8')
   end
 
   # Decodes any data within the specified target data. Upon success, the decoded
   # string will be returned.
   #
   # +target+::  Target data to be decoded
-  def decode(target)
+  def self.decode(target)
     # Convert the target data into an array of bytes
-    bytes = target.unpack("B*")[0].scan(/.{8}/)
+    bytes = target.unpack('B*')[0].scan(/.{8}/)
 
     # Obtain string from the bytes
-    bytes = process_bytes(bytes)
+    bytes = self.process_bytes(bytes)
     return Base64.decode64(bytes)
   end
 
@@ -59,7 +59,7 @@ class Unveiler
   #
   # +bytes+:: Array of bytes to manipulate
   # +data+::  Binary data to insert
-  def manipulate_bytes(bytes, data)
+  def self.manipulate_bytes(bytes, data)
     count = 0
     bit = 7
 
@@ -74,18 +74,20 @@ class Unveiler
       # Decrease the index of the bit to manipulate
       bit -= 1
     end
-    return bytes
+
+    # Ran out of space to encode data
+    raise 'Input file is too small for the specified data'
   end
 
   # Processes the specified array of bytes by looping through each byte and
   # appending the least significant bit onto a string of bits. At the end of
   # every eighth bit, a check will be made to see if the last 3 bytes of the
-  # string contain the phrase "EOF". Only the bytes before EOF will be returned
+  # string contain the phrase 'EOF'. Only the bytes before EOF will be returned
   # if the phrase is found. An error will be raised if no EOF is found.
   #
   # +bytes+:: Array of bytes to be processed
-  def process_bytes(bytes)
-    bits = ""
+  def self.process_bytes(bytes)
+    bits = ''
     index = 7
 
     while index >= 0
@@ -101,11 +103,11 @@ class Unveiler
           if data.length >= 3
             # Convert bytes to a UTF-8 string
             data.map!{|byte| byte = byte.to_i(2)}
-            data = data.pack("C*").force_encoding("utf-8")
+            data = data.pack('C*').force_encoding('utf-8')
             len = data.length
 
-            if data[-3,3] == "EOF"
-              # Return the UTF-8 string, excluding "EOF"
+            if data[-3,3] == 'EOF'
+              # Return the UTF-8 string, excluding 'EOF'
               return data[0..len - 3]
             end
           end
@@ -116,6 +118,6 @@ class Unveiler
     end
 
     # No EOF was found, error
-    raise "No phrase matching 'EOF' was found"
+    raise 'No phrase matching "EOF" was found'
   end
 end
--- spec/manipulate_spec.rb
+++ spec/manipulate_spec.rb
@@ -1,111 +1,113 @@
-require "unveiler"
+require 'unveiler'
 
-RSpec.describe Unveiler, "#manipulate_bytes" do
-  context "with equal number of bytes in string and array" do
-    it "replaces the least significant bits with 0" do
-      unveiler = Unveiler.new
-      data = "0000"
+RSpec.describe Unveiler, '#manipulate_bytes' do
+  context 'with equal number of bytes in string and array' do
+    it 'replaces the least significant bits with 0' do
+      data = '0000'
 
-      array = ["10001000", "10101010", "11111111", "11111111"]
-      answer = ["10001000", "10101010", "11111110", "11111110"]
-      result = unveiler.manipulate_bytes(array, data)
-
-      expect(result.length).to eq 4
-      expect(result).to eq answer
-    end
-    it "replaces the least significant bits with 1" do
-      unveiler = Unveiler.new
-      data = "1111"
-
-      array = ["10001000", "10101010", "11111111", "11111110"]
-      answer = ["10001001", "10101011", "11111111", "11111111"]
-      result = unveiler.manipulate_bytes(array, data)
+      array = ['10001000', '10101010', '11111111', '11111111']
+      answer = ['10001000', '10101010', '11111110', '11111110']
+      result = Unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
     end
-    it "replaces the least significant bits with differing values" do
-      unveiler = Unveiler.new
-      data = "1010"
+    it 'replaces the least significant bits with 1' do
+      data = '1111'
 
-      array = ["10001000", "10101010", "11111111", "11111110"]
-      answer = ["10001000", "10101011", "11111110", "11111111"]
-      result = unveiler.manipulate_bytes(array, data)
+      array = ['10001000', '10101010', '11111111', '11111110']
+      answer = ['10001001', '10101011', '11111111', '11111111']
+      result = Unveiler.manipulate_bytes(array, data)
+
+      expect(result.length).to eq 4
+      expect(result).to eq answer
+    end
+    it 'replaces the least significant bits with differing values' do
+      data = '1010'
+
+      array = ['10001000', '10101010', '11111111', '11111110']
+      answer = ['10001000', '10101011', '11111110', '11111111']
+      result = Unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
     end
   end
 
-  context "with a larger array length than the data string" do
-    it "replaces the least significant bits with 0" do
-      unveiler = Unveiler.new
-      data = "00"
+  context 'with a larger array length than the data string' do
+    it 'replaces the least significant bits with 0' do
+      data = '00'
 
-      array = ["10001001", "10101011", "11111111", "11111111"]
-      answer = ["10001001", "10101011", "11111110", "11111110"]
-      result = unveiler.manipulate_bytes(array, data)
-
-      expect(result.length).to eq 4
-      expect(result).to eq answer
-    end
-    it "replaces the least significant bits with 1" do
-      unveiler = Unveiler.new
-      data = "11"
-
-      array = ["10001000", "10101010", "11111111", "11111111"]
-      answer = ["10001000", "10101010", "11111111", "11111111"]
-      result = unveiler.manipulate_bytes(array, data)
+      array = ['10001001', '10101011', '11111111', '11111111']
+      answer = ['10001001', '10101011', '11111110', '11111110']
+      result = Unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
     end
-    it "replaces the least significant bits with differing values" do
-      unveiler = Unveiler.new
-      data = "10"
+    it 'replaces the least significant bits with 1' do
+      data = '11'
 
-      array = ["10001001", "10101010", "11111111", "11111110"]
-      answer = ["10001001", "10101010", "11111110", "11111111"]
-      result = unveiler.manipulate_bytes(array, data)
+      array = ['10001000', '10101010', '11111111', '11111111']
+      answer = ['10001000', '10101010', '11111111', '11111111']
+      result = Unveiler.manipulate_bytes(array, data)
+
+      expect(result.length).to eq 4
+      expect(result).to eq answer
+    end
+    it 'replaces the least significant bits with differing values' do
+      data = '10'
+
+      array = ['10001001', '10101010', '11111111', '11111110']
+      answer = ['10001001', '10101010', '11111110', '11111111']
+      result = Unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
     end
   end
 
-  context "with a longer data string than the array length" do
-    it "replaces the least significant bits with 0" do
-      unveiler = Unveiler.new
-      data = "000000"
+  context 'with a longer data string than the array length' do
+    it 'replaces the least significant bits with 0' do
+      data = '000000'
 
-      array = ["11111111", "11111111", "11111111", "11111111"]
-      answer = ["11111110", "11111110", "11111100", "11111100"]
-      result = unveiler.manipulate_bytes(array, data)
-
-      expect(result.length).to eq 4
-      expect(result).to eq answer
-    end
-    it "replaces the least significant bits with 1" do
-      unveiler = Unveiler.new
-      data = "111111"
-
-      array = ["11111110", "11111110", "11111110", "11111110"]
-      answer = ["11111111", "11111111", "11111111", "11111111"]
-      result = unveiler.manipulate_bytes(array, data)
+      array = ['11111111', '11111111', '11111111', '11111111']
+      answer = ['11111110', '11111110', '11111100', '11111100']
+      result = Unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
     end
-    it "replaces the least significant bits with differing values" do
-      unveiler = Unveiler.new
-      data = "100101"
+    it 'replaces the least significant bits with 1' do
+      data = '111111'
 
-      array = ["10001000", "10101011", "11111111", "11111110"]
-      answer = ["10001001", "10101010", "11111110", "11111101"]
-      result = unveiler.manipulate_bytes(array, data)
+      array = ['11111110', '11111110', '11111110', '11111110']
+      answer = ['11111111', '11111111', '11111111', '11111111']
+      result = Unveiler.manipulate_bytes(array, data)
 
       expect(result.length).to eq 4
       expect(result).to eq answer
+    end
+    it 'replaces the least significant bits with differing values' do
+      data = '100101'
+
+      array = ['10001000', '10101011', '11111111', '11111110']
+      answer = ['10001001', '10101010', '11111110', '11111101']
+      result = Unveiler.manipulate_bytes(array, data)
+
+      expect(result.length).to eq 4
+      expect(result).to eq answer
+    end
+    it 'throws an error if the data is too large' do
+      begin
+        data = '0000000000000000'
+        bytes = ['11111111']
+
+        result = Unveiler.manipulate_bytes(bytes, data)
+        fail
+      rescue
+        # Should fail
+      end
     end
   end
 end
