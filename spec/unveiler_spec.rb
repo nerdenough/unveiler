a8c97dc20522630bf6e7f63a903ee3efc3e2b6c8
Brendan Goodenough <brendan@goodenough.nz>
2016-05-21 12:10:02 +1200

    Move Unveiler tests to same file

--- spec/unveiler_spec.rb
+++ spec/unveiler_spec.rb
@@ -1,11 +1,54 @@
 require 'unveiler'
+require 'base64'
 
 RSpec.describe Unveiler do
-  before(:example) do
-    @input_bytes = ['10001000', '10101010', '11111111', '00000000']
+  describe '#encode' do
+    before(:example) do
+      @target = 'Hello, world!'
+      @data = 'hi'
+    end
+
+    context 'when the data is smaller than the target' do
+      it 'should return the encoded data as a string' do
+        result = Unveiler.encode(@target, @data)
+        expect(result.is_a? String).to eq true
+      end
+
+      it 'should return a string of equal length to the target' do
+        result = Unveiler.encode(@target, @data)
+        expect(result.length).to eq @target.length
+      end
+
+      it 'should encode the data as base64 into the target' do
+        result = Unveiler.encode(@target, @data)
+        result = result.unpack('B*')[0].scan(/.{8}/)
+        result = Unveiler.process_bytes(result)
+        expect(Base64.decode64(result)).to eq @data
+      end
+    end
+
+    context 'when the data is larger than the target' do
+      it 'should raise a RuntimeError' do
+        expect{Unveiler.encode(@data, @target)}.to raise_error(RuntimeError)
+      end
+    end
+
+    context 'when the wrong arguments are passed' do
+      it 'should raise an ArgumentError if the target is not a String' do
+        expect{Unveiler.encode(true, @data)}.to raise_error(ArgumentError)
+      end
+
+      it 'should raise an ArgumentError if the data is not a String' do
+        expect{Unveiler.encode(@target, [1, 2])}.to raise_error(ArgumentError)
+      end
+    end
   end
 
   describe '#manipulate_bytes' do
+    before(:example) do
+      @input_bytes = ['10001000', '10101010', '11111111', '00000000']
+    end
+
     context 'when the data length matches the array size' do
       it 'should only replace the least significant bits' do
         data = '1111'
@@ -55,14 +98,9 @@ RSpec.describe Unveiler do
     end
 
     context 'when the data length is larger than the array size' do
-      it 'should raise an error' do
-        begin
-          data = '111111111111111111111111111111111'
-          Unveiler.manipulate_bytes(@input_bytes, data)
-          fail
-        rescue
-          # Should have raised an error
-        end
+      it 'should raise a RuntimeError' do
+        data = '111111111111111111111111111111111'
+        expect{Unveiler.manipulate_bytes(@input_bytes, data)}.to raise_error(RuntimeError)
       end
     end
   end
